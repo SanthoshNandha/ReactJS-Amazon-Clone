a9bbb39a8e6f330b8a6f4935eca42cf5ee4e826a
Santhosh Nandhakumar <nsanthosh2409@gmail.com>
2020-09-15 14:34:52 -0500

    Login style changes

--- src/Login.js
+++ src/Login.js
@@ -12,30 +12,26 @@ function Login() {
 
     const login = (event) => {
         event.preventDefault();
-
         auth.signInWithEmailAndPassword(email, password)
-            .then((auth) => {
-                // logged in redirect to homepage
-                history.push("/");
-            })
-            .catch((e) => {
-                alert(e.message);
-            })
-
+        .then((auth) => {
+            // logged in redirect to homepage
+            history.push("/");
+        })
+        .catch((e) => {
+            alert(e.message);
+        });
     }
 
     const register = (event) => {
         event.preventDefault();
-
         auth.createUserWithEmailAndPassword(email, password)
-            .then((auth) => {
-                // created a user so redirect to homepage
-                history.push("/");
-            })
-            .catch((e) => {
-                alert(e.message);
-            })
-
+        .then((auth) => {
+            // created a user so redirect to homepage
+            history.push("/");
+        })
+        .catch((e) => {
+            alert(e.message);
+        });
     }
 
     return (
@@ -53,9 +49,13 @@ function Login() {
                     <input value = {email} onChange = {event => setEmail(event.target.value)} type="text" name="email"/>
                     <h5>Password</h5>
                     <input value={password} onChange = {event =>  setPassword(event.target.value)} type="password" name="password" />
-                    <button onClick={login} type="submit">Sign In</button>
-                    <button onClick={register}>Create your Amazon account</button>
+                    <button className="login__signInButton" onClick={login} type="submit">Sign In</button>
                 </form>
+                <p>
+                    By signing-in you agree to Amazon's Conditions of Use & Sale. Please see our Privacy Notice, 
+                    our Cookies Notice and our Interest-Based Ads Notice.
+                </p>
+                <button className="login__registerButton" onClick={register}>Create your Amazon account</button>
             </div>
         </div>
     )
